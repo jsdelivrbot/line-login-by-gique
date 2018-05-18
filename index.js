68a9e7128915a13ac151038d38040014bd9ee64d
Gique <sakul.mon@ascendcorp.com>
2018-05-18 14:39:52 +0700

    do the login

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 "use strict";
 
-const LINE_LOGIN_CHANNEL_ID=1581612706;
-const LINE_LOGIN_CHANNEL_SECRET=ca9a8fdaa2e65203b3685c37277ac5c8;
-const LINE_LOGIN_CALLBACK_URL="https://line-login-by-gique.herokuapp.com/callback";
-
+const dotenv = require('dotenv').config();
 const app = require('express')();
 const line_login = require("line-login");
 const session = require("express-session");
