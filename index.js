f6ee4741393238bea9963ba07dcc2693c20ff1b3
Gique <sakul.mon@ascendcorp.com>
2018-05-18 14:35:18 +0700

    do the login

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 "use strict";
- 
+
+const LINE_LOGIN_CHANNEL_ID=1581612706;
+const LINE_LOGIN_CHANNEL_SECRET=ca9a8fdaa2e65203b3685c37277ac5c8;
+const LINE_LOGIN_CALLBACK_URL="https://line-login-by-gique.herokuapp.com/callback";
+
 const app = require('express')();
 const line_login = require("line-login");
 const session = require("express-session");
 const session_options = {
-    secret: "ca9a8fdaa2e65203b3685c37277ac5c8",
+    secret: process.env.LINE_LOGIN_CHANNEL_SECRET,
     resave: false,
     saveUninitialized: false
 }
 app.use(session(session_options));
 
 const login = new line_login({
-    channel_id: "1581612706",
-    channel_secret: "ca9a8fdaa2e65203b3685c37277ac5c8",
-    callback_url: "https://line-login-by-gique.herokuapp.com/callback",
-    //scope: "openid profile",
+    channel_id: process.env.LINE_LOGIN_CHANNEL_ID,
+    channel_secret: process.env.LINE_LOGIN_CHANNEL_SECRET,
+    callback_url: process.env.LINE_LOGIN_CALLBACK_URL,
+    scope: "openid profile",
     prompt: "consent",
     bot_prompt: "normal"
 });
@@ -26,7 +30,7 @@ app.listen(process.env.PORT || 5000, () => {
 app.use("/", login.auth());
  
 // Specify the path you want to wait for the callback from LINE authorization endpoint.
-app.use("/callbacks", login.callback(
+app.use("/callback", login.callback(
     (req, res, next, token_response) => {
         // Success callback
         console.log("Success");
